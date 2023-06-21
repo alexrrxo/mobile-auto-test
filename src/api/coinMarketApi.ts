335c113d7417513cca779a13fa9bc38cfa34df66
alexrrxo <alexrrxo@gmail.com>
2023-06-21 18:26:33 +0300

    add api

--- src/api/coinMarketApi.ts
+++ src/api/coinMarketApi.ts
@@ -1,3 +1,26 @@
 import axios from 'axios';
 
-class coinMarketApi {}
+const instance = axios.create({
+  headers: {
+    'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
+  },
+  baseURL:
+    'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest',
+  params: {
+    symbol: 'BTC',
+  },
+});
+
+class CoinMarketApi {
+  async getData() {
+    try {
+      debugger;
+      const response = await instance.get('');
+      return response;
+    } catch (error) {
+      return error;
+    }
+  }
+}
+
+export const coinMarketApi = new CoinMarketApi();
--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import IntervalBar from '../IntervalBar';
 import Tables from '../Tables';
 import Pagination from '../Pagination';
+import { coinMarketApi } from '../../api/coinMarketApi';
 
 const Board = () => {
+  const getData = async () => {
+    const response = await coinMarketApi.getData();
+    console.log(response);
+  };
+
   return (
     <div>
       <IntervalBar />
       <Tables />
       <Pagination />
+
+      <button onClick={getData}>get data from coinApi</button>
     </div>
   );
 };
