812120f0e5166884eede7b9b914274251b83692f
alexrrxo <alexrrxo@gmail.com>
2023-06-23 16:02:41 +0300

    complete

--- src/api/coinMarketApi.ts
+++ src/api/coinMarketApi.ts
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  headers: {
-    'X-CMC_PRO_API_KEY': '43d28735-e5b2-4456-890b-25d0c9e70c6a',
-  },
   baseURL:
     'https://coin-realy.mobilauto.com.ua/v1/cryptocurrency/quotes/latest',
   params: {
     symbol: 'BTC',
+    CMC_PRO_API_KEY: '43d28735-e5b2-4456-890b-25d0c9e70c6a',
   },
 });
 
--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -2,23 +2,13 @@ import IntervalBar from '../IntervalBar';
 import Tables from '../Tables';
 import Pagination from '../Pagination';
 import './Board.css';
-import { useDispatch } from 'react-redux';
-import { addData } from '../../store/reducers/crypto.reducer';
-import { useAppDispatch } from '../../hooks';
 
 const Board = () => {
-  const dispatch = useAppDispatch();
-
-  const clickHandler = () => {
-    dispatch(addData());
-  };
   return (
     <div className="board">
       <IntervalBar />
       <Tables />
       <Pagination />
-
-      <button onClick={clickHandler}>send</button>
     </div>
   );
 };
